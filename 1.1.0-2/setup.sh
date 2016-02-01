cbae750b7dd902603a994fb465838b021db1b858
Yuyang Dong <yudo@microsoft.com>
2016-02-01 10:59:29 -0800

    Fix docker build

--- 1.1.0-2/setup.sh
+++ 1.1.0-2/setup.sh
@@ -6,7 +6,7 @@ mv /root/omsbundle* /root/omsbundle
 /usr/bin/dpkg -i /root/omsbundle/100/scx*.deb
 /usr/bin/dpkg -i /root/omsbundle/100/omsagent*.deb
 /usr/bin/dpkg -i /root/omsbundle/100/omsconfig*.deb
-/root/omsbundle/oss-kits/docker-cimprov-1.0.0-0.universal.x86_64.sh --install
+/root/docker-cimprov-1.0.0-0.universal.x86_64.sh --install
 rm -rf /root/omsbundle
 rm -f /root/omsagent*.sh
 rm -f /root/*.deb
