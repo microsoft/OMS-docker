e30e00703f47dd557f6ab05e7299274b38448e72
Yuyang Dong <yudo@microsoft.com>
2016-02-01 09:48:38 -0800

    Update sources

--- 1.1.0-2/setup.sh
+++ 1.1.0-2/setup.sh
@@ -1,4 +1,4 @@
-wget https://github.com/MSFTOSSMgmt/omsagent/blob/develop/releases/download/1.1.0-2/omsagent-1.1.0-2.universal.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/v1.1.0-2/omsagent-1.1.0-2.universal.x64.sh
 chmod 775 /root/*.sh
 /root/omsagent-1.1.0-2.universal.x64.sh --extract
 mv /root/omsbundle* /root/omsbundle
@@ -6,7 +6,7 @@ mv /root/omsbundle* /root/omsbundle
 /usr/bin/dpkg -i /root/omsbundle/100/scx*.deb
 /usr/bin/dpkg -i /root/omsbundle/100/omsagent*.deb
 /usr/bin/dpkg -i /root/omsbundle/100/omsconfig*.deb
-/root/omsbundle/oss-kits/docker-cimprov-0.1.0-0.universal.x86_64.sh --install
+/root/omsbundle/oss-kits/docker-cimprov-1.0.0-0.universal.x86_64.sh --install
 rm -rf /root/omsbundle
 rm -f /root/omsagent*.sh
 rm -f /root/*.deb
