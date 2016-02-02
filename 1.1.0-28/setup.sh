5f5386579c55b5b828b4d38a9d2a913653e9b901
Yuyang Dong <yudo@microsoft.com>
2016-02-02 14:04:48 -0800

    Update for new OMS agent version

---
+++ 1.1.0-28/setup.sh
@@ -0,0 +1,12 @@
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/v1.1.0-28/omsagent-1.1.0-28.universal.x64.sh
+chmod 775 /root/*.sh
+/root/omsagent-1.1.0-28.universal.x64.sh --extract
+mv /root/omsbundle* /root/omsbundle
+/usr/bin/dpkg -i /root/omsbundle/100/omi*.deb
+/usr/bin/dpkg -i /root/omsbundle/100/scx*.deb
+/usr/bin/dpkg -i /root/omsbundle/100/omsagent*.deb
+/usr/bin/dpkg -i /root/omsbundle/100/omsconfig*.deb
+/root/docker-cimprov-1.0.0-0.universal.x86_64.sh --install
+rm -rf /root/omsbundle
+rm -f /root/omsagent*.sh
+rm -f /root/*.deb
