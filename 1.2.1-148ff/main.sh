9725efdc276ca6629f6fdf882c1d34a8b170571d
Keiko Harada <keikhara@microsoft.com>
2016-11-16 11:57:49 -0800

    ACS Docs (#15)

---
+++ 1.2.1-148ff/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+service omid start
+
+if [ -z $INT ]; then
+	/opt/microsoft/omsagent/bin/omsadmin.sh -w $WSID -s $KEY -d $DOMAIN
+else
+	echo WORKSPACE_ID=$WSID > /etc/omsagent-onboard.conf
+	echo SHARED_KEY=$KEY >> /etc/omsagent-onboard.conf
+	/opt/microsoft/omsagent/bin/omsadmin.sh
+fi
+
+sed -i -e 's/^  bind 127.0.0.1$/  bind 0.0.0.0/g' /etc/opt/microsoft/omsagent/conf/omsagent.conf
+sed -i -e 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/opt/microsoft/omsagent/conf/omsagent.d/container.conf
+
+service omsagent start
+
+/opt/microsoft/omsconfig/Scripts/OMS_MetaConfigHelper.py --disable
+rm -f /etc/opt/microsoft/omsagent/conf/omsagent.d/omsconfig.consistencyinvoker.conf
+
+sleep inf
