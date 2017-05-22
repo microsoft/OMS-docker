95ca7baed1802b208f06b9c67bd6c2cad1e44b4f
Sambit Mishra <samis@microsoft.com>
2017-05-22 11:51:24 -0700

    Hostname path change
    using /var/opt/microsoft/docker-cimprov/state instead of /var/opt/microsoft/omsagent/state since the latter gets deleted during onboarding

--- 1.3.4-127a/main.sh
+++ 1.3.4-127a/main.sh
@@ -7,6 +7,11 @@ sed -i -e 's/^exit 101$/exit 0/g' /usr/sbin/policy-rc.d
 #Using the get_hostname for hostname instead of the host field in syslog messages
 sed -i.bak "s/record\[\"Host\"\] = hostname/record\[\"Host\"\] = OMS::Common.get_hostname/" /opt/microsoft/omsagent/plugin/filter_syslog.rb
 
+#using /var/opt/microsoft/docker-cimprov/state instead of /var/opt/microsoft/omsagent/state since the latter gets deleted during onboarding
+mkdir -p /var/opt/microsoft/docker-cimprov/state
+curl --unix-socket /var/run/docker.sock "http:/info" | python -c "import sys, json; print json.load(sys.stdin)['Name']" > /var/opt/microsoft/docker-cimprov/state/containerhostname
+sed -i.bak "s/\/var\/opt\/microsoft\/omsagent\/state\/containerhostname/\/var\/opt\/microsoft\/docker\-cimprov\/state\/containerhostname/" /opt/microsoft/omsagent/plugin/oms_common.rb
+
 #service omid start
 /opt/omi/bin/service_control start
 
