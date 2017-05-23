2969d38cea749c793fedbbe672be1bb9ae676456
Sambit Mishra <samis@microsoft.com>
2017-05-23 14:18:30 -0700

    Adding omsagent v1.3.4-127 and docker-provider 1.0.0-24

--- 1.3.4-127a/setup.sh
+++ 1.3.4-127a/setup.sh
@@ -1,15 +1,16 @@
 TMPDIR="/opt"
 cd $TMPDIR
-#wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent-201704-v1.3.4-15/omsagent-1.3.4-15.universal.x64.sh
-wget https://keiko.blob.core.windows.net/container/omsagent-1.3.4-127.universal.x64.sh
+wget https://aka.ms/oms-linux-agent-latest
+wget https://aka.ms/docker-provider-latest
 chmod 775 $TMPDIR/*.sh
-$TMPDIR/omsagent-1.3.4-*.universal.x64.sh --extract
+$TMPDIR/omsagent-*.universal.x64.sh --extract
 mv $TMPDIR/omsbundle* $TMPDIR/omsbundle
 $TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsagent*.deb
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
-/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
-#/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
+#/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
+#Use downloaded docker-provider instead of the bundled one
+/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
 rm -rf $TMPDIR/omsbundle
 rm -f $TMPDIR/omsagent*.sh
 rm -f $TMPDIR/docker-cimprov*.sh
