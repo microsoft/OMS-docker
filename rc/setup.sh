70950421935babf9306080c7d7f7da06b76449ad
Sambit Mishra <samis@microsoft.com>
2017-08-19 13:26:27 -0700

    upgrade omi

--- rc/setup.sh
+++ rc/setup.sh
@@ -1,6 +1,6 @@
 TMPDIR="/opt"
 cd $TMPDIR
-wget https://samisstorage.blob.core.windows.net/omsagentrctest/omsagent-1.4.0-12.universal.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.0-45/omsagent-1.4.0-45.universal.x64.sh
 wget https://samisstorage.blob.core.windows.net/dockerproviderreleases/docker-cimprov-1.0.0-27.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
@@ -9,6 +9,15 @@ mv $TMPDIR/omsbundle* $TMPDIR/omsbundle
 $TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsagent*.deb
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
+
+#Upgrade OMI
+wget http://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get upgrade -y omi
+
 #/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
 #Use downloaded docker-provider instead of the bundled one
 /$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
