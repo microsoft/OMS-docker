04aaff2b781ea8f2e9f31981c72c2152d750c4b0
Sambit Mishra <samis@microsoft.com>
2017-08-19 14:50:18 -0700

    upgrade omi

--- rc/main.sh
+++ rc/main.sh
@@ -42,7 +42,12 @@ else
 fi
 
 #Hack for omi upgrade
- /usr/bin/apt-get upgrade -y omi
+wget http://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+ dpkg -i packages-microsoft-prod.deb
+ apt-get update
+ apt-get install -y apt-transport-https
+ apt-get update
+ apt-get upgrade -y omi
 
 
  /opt/omi/bin/omiserver -s
