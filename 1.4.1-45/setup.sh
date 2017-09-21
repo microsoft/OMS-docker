32ce77064527930b6d55b097450bca5d2b103856
Sambit Mishra <samis@microsoft.com>
2017-09-21 14:37:04 -0700

    kubeapi fix test

--- 1.4.1-45/setup.sh
+++ 1.4.1-45/setup.sh
@@ -1,6 +1,6 @@
 TMPDIR="/opt"
 cd $TMPDIR
-wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.0-45/omsagent-1.4.0-45.universal.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.1-45/omsagent-1.4.1-45.universal.x64.sh
 wget https://samisstorage.blob.core.windows.net/testdockerprovider/docker-cimprov-1.0.0-28.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
