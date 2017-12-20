eb2621290c5711960e47e98e8f2fd5538c416127
Sambit Mishra <samis@microsoft.com>
2017-12-19 22:44:44 -0800

    adding an image for latest omi

---
+++ latestomi/setup.sh
@@ -0,0 +1,18 @@
+TMPDIR="/opt"
+cd $TMPDIR
+#wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.1-45/omsagent-1.4.1-45.universal.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.1-123/omsagent-1.4.1-123.universal.x64.sh
+wget https://samisstorage.blob.core.windows.net/testdockerprovider/docker-cimprov-1.0.0-30.universal.x86_64.sh
+chmod 775 $TMPDIR/*.sh
+
+$TMPDIR/omsagent-*.universal.x64.sh --extract
+mv $TMPDIR/omsbundle* $TMPDIR/omsbundle
+$TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsagent*.deb
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
+#/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
+#Use downloaded docker-provider instead of the bundled one
+/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
+rm -rf $TMPDIR/omsbundle
+rm -f $TMPDIR/omsagent*.sh
+rm -f $TMPDIR/docker-cimprov*.sh
