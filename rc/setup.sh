782d574240725f5df60f7f4c5ba4e5412b5ac5ad
bragi92 <kadubey@microsoft.com>
2018-01-23 09:50:53 -0800

    Update setup.sh

--- rc/setup.sh
+++ rc/setup.sh
@@ -1,7 +1,8 @@
 TMPDIR="/opt"
 cd $TMPDIR
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.3-174/omsagent-1.4.3-174.universal.x64.sh 
-wget https://samisstorage.blob.core.windows.net/testkaveeshcontainer/docker-cimprov-1.0.0-29.universal.x86_64.sh
+#wget https://samisstorage.blob.core.windows.net/testkaveeshcontainer/docker-cimprov-1.0.0-29.universal.x86_64.sh
+wget https://github.com/Microsoft/Docker-Provider/releases/download/1.0.0-30/docker-cimprov-1.0.0-30.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 $TMPDIR/omsagent-*.universal.x64.sh --extract
