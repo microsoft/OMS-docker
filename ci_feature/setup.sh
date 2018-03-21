6b118bb22b842f9139de23655e2b1e1757d48745
Vishwanath <visnara@microsoft.com>
2018-03-20 18:55:04 -0700

    updating docker provider to a private build.

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -1,7 +1,7 @@
 TMPDIR="/opt"
 cd $TMPDIR
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.3-174/omsagent-1.4.3-174.universal.x64.sh 
-wget https://samisstorage.blob.core.windows.net/testdockerprovider/docker-cimprov-2.0.0-5.universal.x86_64.sh
+wget https://vishwasstorageaccount.blob.core.windows.net/dockerprovider0320/docker-cimprov-1.0.0-31.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 $TMPDIR/omsagent-*.universal.x64.sh --extract
@@ -11,7 +11,7 @@ $TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
 #/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
 #Use downloaded docker-provider instead of the bundled one
-/$TMPDIR/docker-cimprov-2.0.0-*.x86_64.sh --install
+/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
 rm -rf $TMPDIR/omsbundle
 rm -f $TMPDIR/omsagent*.sh
 rm -f $TMPDIR/docker-cimprov*.sh
