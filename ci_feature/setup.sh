8f8674768cc79774b19f84417f700c6e50eb62a9
Sambit Mishra <samis@microsoft.com>
2018-03-27 11:26:31 -0700

    Update docker-provider to 2.0.0-6

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -11,7 +11,7 @@ $TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
 #/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
 #Use downloaded docker-provider instead of the bundled one
-/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
+/$TMPDIR/docker-cimprov-2.0.0-*.x86_64.sh --install
 rm -rf $TMPDIR/omsbundle
 rm -f $TMPDIR/omsagent*.sh
 rm -f $TMPDIR/docker-cimprov*.sh
