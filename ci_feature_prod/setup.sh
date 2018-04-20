ab9669f5c59aae326bcc6d9016806ceffc6a893d
Vishwanath <visnara@microsoft.com>
2018-04-20 13:45:11 -0700

    Vishwa/cirelease (#105)
    * addinf ci_feature_prod
    
    * copy over from ci_feature (baseline)

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -1,17 +1,32 @@
 TMPDIR="/opt"
 cd $TMPDIR
-wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.3-174/omsagent-1.4.3-174.universal.x64.sh 
-wget https://vishwasstorageaccount.blob.core.windows.net/dockerprovider0327-2/docker-cimprov-2.0.0-6.universal.x86_64.sh
+
+wget https://vishwasstorageaccount.blob.core.windows.net/omsagent/omsagent-1.6.0-23.universal.x64.sh
+
+#create file to disable omi service startup script
+touch /etc/.omi_disable_service_control
+wget https://vishwasstorageaccount.blob.core.windows.net/dockerprovider0418/docker-cimprov-1.0.0-31.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
+#Extract omsbundle
 $TMPDIR/omsagent-*.universal.x64.sh --extract
 mv $TMPDIR/omsbundle* $TMPDIR/omsbundle
-$TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
+#Install omi
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omi*.deb
+
+#Install scx
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/scx*.deb
+#$TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
+
+#Install omsagent and omsconfig
+
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsagent*.deb
 /usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
 #/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
 #Use downloaded docker-provider instead of the bundled one
-/$TMPDIR/docker-cimprov-2.0.0-*.x86_64.sh --install
+
+/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
+
 rm -rf $TMPDIR/omsbundle
 rm -f $TMPDIR/omsagent*.sh
 rm -f $TMPDIR/docker-cimprov*.sh
