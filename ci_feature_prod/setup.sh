20da85c0b5ded5523bf671cd4011453118da9cde
Vishwanath <visnara@microsoft.com>
2018-05-16 13:39:13 -0700

    Ci feature prod (#115)
    * addinf ci_feature_prod (#104)
    
    * Vishwa/cirelease (#105)
    
    * addinf ci_feature_prod
    
    * copy over from ci_feature (baseline)
    
    * Vishwa/cirelease (#106)
    
    * addinf ci_feature_prod
    
    * copy over from ci_feature (baseline)
    
    * * update docker provider version
    * update docker provider package to a release in dockerprovider repo
    
    * Updating oms agent to v1.6.0-23 release (#107)
    
    * Reflect right versions and set imagepllpolicy to IfNotPresent (#109)
    
    * Vishwa/rbac (#113)
    
    * Reflect right versions and set imagepllpolicy to IfNotPresent
    
    * rbac support
    
    * enabling rbac
    
    * updating verb from get to list
    
    * changing from v1 to vibeta for cluster role and cluster role binding
    
    * updating yaml with correct dockerprovider, omsagent & iomage tag versions
    
    * updating oms agent version and docker provider version
    
    * Fixing a telemetry issue
    
    * updating yaml
    
    * updating cluster name

---
+++ ci_feature_prod/setup.sh
@@ -0,0 +1,32 @@
+TMPDIR="/opt"
+cd $TMPDIR
+
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.6.0-42/omsagent-1.6.0-42.universal.x64.sh
+
+#create file to disable omi service startup script
+touch /etc/.omi_disable_service_control
+wget https://github.com/Microsoft/Docker-Provider/releases/download/2.0.0-2/docker-cimprov-2.0.0-2.universal.x86_64.sh
+chmod 775 $TMPDIR/*.sh
+
+#Extract omsbundle
+$TMPDIR/omsagent-*.universal.x64.sh --extract
+mv $TMPDIR/omsbundle* $TMPDIR/omsbundle
+#Install omi
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omi*.deb
+
+#Install scx
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/scx*.deb
+#$TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
+
+#Install omsagent and omsconfig
+
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsagent*.deb
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
+#/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
+#Use downloaded docker-provider instead of the bundled one
+
+/$TMPDIR/docker-cimprov-2.0.0-*.x86_64.sh --install
+
+rm -rf $TMPDIR/omsbundle
+rm -f $TMPDIR/omsagent*.sh
+rm -f $TMPDIR/docker-cimprov*.sh
