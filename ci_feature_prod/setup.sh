8fbff12f87a2bf723753338aeee563f904f86d7a
Vishwanath Narasimhan <visnara@microsoft.com>
2018-08-01 09:31:02 -0700

    updating docker provider build & oms agent build to latest official.

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -1,11 +1,11 @@
 TMPDIR="/opt"
 cd $TMPDIR
 
-wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.6.0-42/omsagent-1.6.0-42.universal.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.6.0-163/omsagent-1.6.0-163.universal.x64.sh
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
-wget https://github.com/Microsoft/Docker-Provider/releases/download/2.0.0-3/docker-cimprov-2.0.0-3.universal.x86_64.sh
+wget https://github.com/Microsoft/Docker-Provider/releases/download/2.0.0-4/docker-cimprov-2.0.0-4.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 #Extract omsbundle
