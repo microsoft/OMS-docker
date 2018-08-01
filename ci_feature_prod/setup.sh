6dcf97aff52f9e38180e0729c2d4e8c91b6cde3b
Vishwanath Narasimhan <visnara@microsoft.com>
2018-08-01 10:34:02 -0700

    adding a missing change into ci_feature_prod folder for locale issue

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -1,6 +1,14 @@
 TMPDIR="/opt"
 cd $TMPDIR
 
+#Download utf-8 encoding capability on the omsagent container.
+
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y locales
+
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+    dpkg-reconfigure --frontend=noninteractive locales && \
+    update-locale LANG=en_US.UTF-8
+
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.6.0-163/omsagent-1.6.0-163.universal.x64.sh
 
 #create file to disable omi service startup script
