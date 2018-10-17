26600fb76dc195e2600ee5c1d217b28b848a8658
Vishwanath <visnara@microsoft.com>
2018-10-16 19:41:14 -0700

    updating docker provider build

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -13,7 +13,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
-wget https://github.com/Microsoft/Docker-Provider/releases/download/3.0.0-0/docker-cimprov-3.0.0-0.universal.x86_64.sh
+wget https://github.com/Microsoft/Docker-Provider/releases/download/3.0.0-1/docker-cimprov-3.0.0-1.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
 
