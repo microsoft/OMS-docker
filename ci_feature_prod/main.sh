6b565a43909a93f338a17512ee59f5c198df2a6c
Vishwanath <visnara@microsoft.com>
2018-11-27 15:46:08 -0800

    move the call to setfacl before first docker.sock call.

--- ci_feature_prod/main.sh
+++ ci_feature_prod/main.sh
@@ -13,6 +13,10 @@ sed -i.bak "s/record\[\"Host\"\] = hostname/record\[\"Host\"\] = OMS::Common.get
 
 #using /var/opt/microsoft/docker-cimprov/state instead of /var/opt/microsoft/omsagent/state since the latter gets deleted during onboarding
 mkdir -p /var/opt/microsoft/docker-cimprov/state
+
+# add permissions for omsagent user to docker.sock
+sudo setfacl -m user:omsagent:rw /var/run/docker.sock
+
 if [[ "$KUBERNETES_SERVICE_HOST" ]];then
 	#kubernetes treats node names as lower case
 	curl --unix-socket /var/run/docker.sock "http:/info" | python -c "import sys, json; print json.load(sys.stdin)['Name'].lower()" > /var/opt/microsoft/docker-cimprov/state/containerhostname
@@ -24,9 +28,6 @@ else
 fi
 #check if file was written successfully
 cat /var/opt/microsoft/docker-cimprov/state/containerhostname 
-
-# add permissions for omsagent user to docker.sock
-sudo setfacl -m user:omsagent:rw /var/run/docker.sock
 
 #dump OMS agent Image tag for telemetry purpose
 #step-1 dump the oms agent container imageID
