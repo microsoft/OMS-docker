96eabe0ca9b74ce1666b78459e3aea049486dc0f
rashmy <rashmy@RASHMY-PC2>
2018-12-06 18:04:35 -0800

    reverting docker bundle changes

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -13,7 +13,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
-wget https://rashmi.blob.core.windows.net/disable-omi/docker-cimprov-1.0.0-35.universal.x86_64.sh
+wget https://dockerprovider.blob.core.windows.net/cifeature/docker-cimprov-3.0.0-2.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
 
@@ -38,7 +38,7 @@ sudo apt-get install acl
 #/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
 #Use downloaded docker-provider instead of the bundled one
 
-/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
+/$TMPDIR/docker-cimprov-3.0.0-*.x86_64.sh --install
 
 #download and install fluent-bit(td-agent-bit)
 wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
