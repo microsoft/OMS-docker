1a357d2715099c832308833dcc10dc3f09c0ae91
r-dilip <dilip.rangarajan@gmail.com>
2019-02-13 16:04:01 -0800

    PR Feedback

--- ci_feature_prod/main.sh
+++ ci_feature_prod/main.sh
@@ -20,7 +20,7 @@ mkdir -p /var/opt/microsoft/docker-cimprov/state
 #fi
 
 # add permissions for omsagent user to access azure.json
-sudo setfacl -m user:omsagent:rw /etc/kubernetes/azure.json
+sudo setfacl -m user:omsagent:r /etc/kubernetes/host/azure.json
 
 DOCKER_SOCKET=/var/run/host/docker.sock
 DOCKER_GROUP=docker
