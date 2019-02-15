51f4d3ff31ac3f68c316f35970a4834e802f6c9b
Dilip Raghunathan <dilip.rangarajan@gmail.com>
2019-02-15 14:28:23 -0800

    Custom Metrics -- YAMLChanges and startup script changes for omsagent pod (#228) (#232)
    Yaml Changes and container startup changes for CustomMetrics

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -14,7 +14,8 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
 
-wget https://github.com/Microsoft/Docker-Provider/releases/download/hotfix-01292019/docker-cimprov-3.0.0-8.universal.x86_64.sh
+#wget https://github.com/Microsoft/Docker-Provider/releases/download/hotfix-01292019/docker-cimprov-3.0.0-8.universal.x86_64.sh
+wget https://diliprstorage.blob.core.windows.net/mycontainer/docker-cimprov-3.0.0-9.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
 
