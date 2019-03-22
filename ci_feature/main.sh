45e8d110f90c2277f02e7bce6ddb6c6f550efa66
Vishwanath Narasimhan <visnara@microsoft.com>
2019-03-21 19:01:16 -0700

    fix script

--- ci_feature/main.sh
+++ ci_feature/main.sh
@@ -104,7 +104,7 @@ if [ ! -e "/etc/config/kube.conf" ]; then
       telegrafConfFile="/etc/opt/microsoft/docker-cimprov/telegraf.conf"
 
       #set env vars used by telegraf
-      if [ "$AKS_RESOURCE_ID" == ""]; then
+      if [ -z $AKS_RESOURCE_ID ]; then
             telemetry_aks_resource_id=""
             telemetry_aks_region=""
             telemetry_cluster_name=""
