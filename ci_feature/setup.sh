7c8f4b3a01bd0a172b870e12815d959d138a5289
r-dilip <dilip.rangarajan@gmail.com>
2019-04-12 13:23:49 -0700

    Building New Version of the agent

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -9,14 +9,11 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
 
-wget https://dockerprovider.blob.core.windows.net/omsagent/omsagent-1.8.1-422.universal.x64.sh
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.8.1.256/omsagent-1.8.1-256.universal.x64.sh
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
 
-#wget https://github.com/Microsoft/Docker-Provider/releases/download/hotfix-01292019/docker-cimprov-3.0.0-8.universal.x86_64.sh
-
-#wget "https://github.com/Microsoft/Docker-Provider/releases/download/3.0.0-4(2)/docker-cimprov-3.0.0-4.universal.x86_64.sh"
 wget https://diliprstorage.blob.core.windows.net/mycontainer/docker-cimprov-3.0.0-5.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
