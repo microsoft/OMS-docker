58fe1a1293265773ca46fc6bc0c19a534fc53748
Vishwanath <visnara@microsoft.com>
2019-04-16 13:57:19 -0700

    update to latest dockercimprov build

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -14,7 +14,7 @@ wget https://dockerprovider.blob.core.windows.net/omsagent/omsagent-1.10.0-0.uni
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
 
-wget "https://github.com/Microsoft/Docker-Provider/releases/download/4.0.0-0/docker-cimprov-4.0.0-0.universal.x86_64.sh"
+wget https://dockerprovider.blob.core.windows.net/cifeature/docker-cimprov-4.0.0-0.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
 
