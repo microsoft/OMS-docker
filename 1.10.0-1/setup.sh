6e85068cc10cd3e962e05e30fc9e268a7e1ec455
rashmy <rashmy@RASHMY-PC2>
2019-04-24 15:26:35 -0700

    changes for new omsagent

--- 1.10.0-1/setup.sh
+++ 1.10.0-1/setup.sh
@@ -5,7 +5,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
-wget https://github.com/Microsoft/Docker-Provider/releases/download/1.0.0-36/docker-cimprov-1.0.0-36.universal.x86_64.sh
+wget https://github.com/Microsoft/Docker-Provider/releases/download/1.0.0-37/docker-cimprov-1.0.0-37.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 #Extract omsbundle
