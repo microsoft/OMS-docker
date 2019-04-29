fa44b885a9937cebd0f20d536fd1aad5d16ef918
rashmy <rashmy@RASHMY-PC2>
2019-04-29 15:21:39 -0700

    changes

--- ci_feature/livenessprobe.sh
+++ ci_feature/livenessprobe.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 #test to exit non zero value
-
-exit 0
+(ps -ef | grep omsagent | grep -v "grep") && (ps -ef | grep td-agent-bit | grep -v "grep")
+if [ -s "inotifyoutput.txt" ]
+then
+        exit 1
+        # do something as file has data
+else
+        exit 0
+        # do something as file is empty
+fi
