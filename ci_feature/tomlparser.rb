636dd4cd7460401d46b1223f4c5919e57665c7dc
rashmy <rashmy@RASHMY-PC2>
2019-05-08 14:37:06 -0700

    cahnges

--- ci_feature/tomlparser.rb
+++ ci_feature/tomlparser.rb
@@ -11,6 +11,74 @@ require_relative "tomlrb"
 @stderrExcludeNamespaces = []
 @collectClusterEnvVariables = true
 
+# Use parser to parse the configmap toml file to a ruby structure
+def parseConfigMap
+  begin
+    # Check to see if config map is created
+    if (File.file?(@configMapMountPath))
+      puts "config map for settings mounted, parsing values"
+      parsedConfig = Tomlrb.load_file(@configMapMountPath, symbolize_keys: true)
+      puts "Successfully parsed mounted config map"
+      return parsedConfig
+    else
+      puts "config map for settings not mounted, using defaults"
+      return nil
+    end
+  rescue => errorStr
+    puts "Exception while parsing toml config file: #{errorStr}"
+    return nil
+  end
+end
+
+# Use the ruby structure created after config parsing to set the right values to be used as environment variables
+def populateSettingValuesFromConfigMap(parsedConfig)
+  if !parsedConfig.nil? && !parsedConfig[:log_collection_settings].nil?
+    #Get stdout log config settings
+    begin
+      if !parsedConfig[:log_collection_settings][:stdout].nil? && !parsedConfig[:log_collection_settings][:stdout][:enabled].nil?
+        @collectStdoutLogs = parsedConfig[:log_collection_settings][:stdout][:enabled]
+        puts "Using config map setting for stdout log collection"
+        stdoutNamespaces = parsedConfig[:log_collection_settings][:stdout][:exclude_namespaces]
+        if parsedConfig[:log_collection_settings][:stdout][:enabled] && !stdoutNamespaces.nil?
+          stdoutNamespaces.each do |namespace|
+            @stdoutExcludeNamespaces.push(namespace)
+          end
+          puts "Using config map setting for stdout log collection to exclude namespace"
+        end
+      end
+    rescue => errorStr
+      puts "Exception while reading config settings for stdout log collection - #{errorStr}, using defaults"
+    end
+
+    #Get stderr log config settings
+    begin
+      if !parsedConfig[:log_collection_settings][:stderr].nil? && !parsedConfig[:log_collection_settings][:stderr][:enabled].nil?
+        @collectStdErrLogs = parsedConfig[:log_collection_settings][:stderr][:enabled]
+        puts "Using config map setting for stderr log collection"
+        stderrNamespaces = parsedConfig[:log_collection_settings][:stderr][:exclude_namespaces]
+        if parsedConfig[:log_collection_settings][:stderr][:enabled] && !stderrNamespaces.nil?
+          stdoutNamespaces.each do |namespace|
+            @stderrExcludeNamespaces.push(namespace)
+          end
+          puts "Using config map setting for stderr log collection to exclude namespace"
+        end
+      end
+    rescue => errorStr
+      puts "Exception while reading config settings for stderr log collection - #{errorStr}, using defaults"
+    end
+
+    #Get environment variables log config settings
+    begin
+      if !parsedConfig[:log_collection_settings][:env_var].nil? && !parsedConfig[:log_collection_settings][:env_var][:enabled].nil?
+        @collectClusterEnvVariables = parsedConfig[:log_collection_settings][:env_var][:enabled]
+        puts "Using config map setting for cluster level environment variable collection"
+      end
+    rescue => errorStr
+      puts "Exception while reading config settings for cluster level environment variable collection - #{errorStr}, using defaults"
+    end
+  end
+end
+
 configMapSettings = parseConfigMap
 if !configMapSettings.nil?
   populateSettingValuesFromConfigMap(configMapSettings)
@@ -37,72 +105,4 @@ if !file.nil?
   file.close
 else
   puts "Exception while opening file for writing config environment variables"
-end
-
-# Use parser to parse the configmap toml file to a ruby structure
-def parseConfigMap
-  begin
-    # Check to see if config map is created
-    if (File.file?(@configMapMountPath))
-      puts "config map for settings mounted, parsing values"
-      parsedConfig = Tomlrb.load_file(@configMapMountPath, symbolize_keys: true)
-      puts "Successfully parsed mounted config map"
-      return parsedConfig
-    else
-      puts "config map for settings not mounted, using defaults"
-      return nil
-    end
-  rescue => errorStr
-    puts "Exception while parsing toml config file: #{errorStr}"
-    return nil
-  end
-end
-
-# Use the ruby structure created after config parsing to set the right values to be used as environment variables
-def populateSettingValuesFromConfigMap(parsedConfig)
-  if !parsedconfig.nil? && !parsedconfig[:log_collection_settings].nil?
-    #Get stdout log config settings
-    begin
-      if !parsedconfig[:log_collection_settings][:stdout].nil? && !parsedconfig[:log_collection_settings][:stdout][:enabled].nil?
-        @collectStdoutLogs = parsedconfig[:log_collection_settings][:stdout][:enabled]
-        puts "Using config map setting for stdout log collection"
-        stdoutNamespaces = parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces]
-        if parsedconfig[:log_collection_settings][:stdout][:enabled] && !stdoutNamespaces.nil?
-          stdoutNamespaces.each do |namespace|
-            @stdoutExcludeNamespaces.push(namespace)
-          end
-          puts "Using config map setting for stdout log collection to exclude namespace"
-        end
-      end
-    rescue => errorStr
-      puts "Exception while reading config settings for stdout log collection - #{errorStr}, using defaults"
-    end
-
-    #Get stderr log config settings
-    begin
-      if !parsedconfig[:log_collection_settings][:stderr].nil? && !parsedconfig[:log_collection_settings][:stderr][:enabled].nil?
-        @collectStdErrLogs = parsedconfig[:log_collection_settings][:stderr][:enabled]
-        puts "Using config map setting for stderr log collection"
-        stderrNamespaces = parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces]
-        if parsedconfig[:log_collection_settings][:stderr][:enabled] && !stderrNamespaces.nil?
-          stdoutNamespaces.each do |namespace|
-            @stderrExcludeNamespaces.push(namespace)
-          end
-          puts "Using config map setting for stderr log collection to exclude namespace"
-        end
-      end
-    rescue => errorStr
-      puts "Exception while reading config settings for stderr log collection - #{errorStr}, using defaults"
-    end
-
-    #Get environment variables log config settings
-    begin
-      if !parsedconfig[:log_collection_settings][:env_var].nil? && !parsedconfig[:log_collection_settings][:env_var][:enabled].nil?
-        @collectClusterEnvVariables = parsedconfig[:log_collection_settings][:env_var][:enabled]
-        puts "Using config map setting for cluster level environment variable collection"
-      end
-    rescue => errorStr
-      puts "Exception while reading config settings for cluster level environment variable collection - #{errorStr}, using defaults"
-    end
-  end
 end
