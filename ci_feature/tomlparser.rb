85434aa1c57d387c9545ce8e2634f7e33f00821f
rashmy <rashmy@RASHMY-PC2>
2019-05-08 11:36:35 -0700

    cahnges

--- ci_feature/tomlparser.rb
+++ ci_feature/tomlparser.rb
@@ -10,6 +10,7 @@ collectStdErrLogs = true
 stderrExcludeNamespaces = []
 collectClusterEnvVariables = true
 
+# Use parser to parse the configmap toml file to a ruby structure
 begin
   # Check to see if config map is created
   if (File.file?("/etc/config/settings/omsagent-settings"))
@@ -19,45 +20,47 @@ rescue => errorStr
   puts "Exception while parsing toml config file: #{errorStr}"
 end
 
+# Use the ruby structure created after config parsing to set the right values to be used as environment variables
+begin
+  if !parsedconfig.nil? && !parsedconfig[:log_collection_settings].nil?
+    #Get stdout log config settings
+    if !parsedconfig[:log_collection_settings][:stdout].nil? && !parsedconfig[:log_collection_settings][:stdout][:enabled].nil?
+      collectStdoutLogs = parsedconfig[:log_collection_settings][:stdout][:enabled]
+      #file.write("export AZMON_COLLECT_STDOUT_LOGS=#{collectStdoutLogs}\n")
+      puts "Using config map setting for stdout log collection"
+      if parsedconfig[:log_collection_settings][:stdout][:enabled] && !parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces].nil?
+        stdoutExcludeNamespaces = parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces]
+        #file.write("export AZMON_STDOUT_EXCLUDED_NAMESPACES=#{parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces]}\n")
+        puts "Using config map setting for stdout log collection to exclude namespace"
+      end
+    end
+    #Get stderr log config settings
+    if !parsedconfig[:log_collection_settings][:stderr].nil? && !parsedconfig[:log_collection_settings][:stderr][:enabled].nil?
+      collectStdErrLogs = parsedconfig[:log_collection_settings][:stderr][:enabled]
+      #file.write("export AZMON_COLLECT_STDERR_LOGS=#{parsedconfig[:log_collection_settings][:stderr][:enabled]}\n")
+      puts "Using config map setting for stderr log collection"
+      if parsedconfig[:log_collection_settings][:stderr][:enabled] && !parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces].nil?
+        stderrExcludeNamespaces = parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces]
+        #file.write("export AZMON_STDERR_EXCLUDED_NAMESPACES=#{parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces]}\n")
+        puts "Using config map setting for stderr log collection to exclude namespace"
+      end
+    end
+    #Get environment variables log config settings
+    if !parsedconfig[:log_collection_settings][:env_var].nil? && !parsedconfig[:log_collection_settings][:env_var][:enabled].nil?
+      collectClusterEnvVariables = parsedconfig[:log_collection_settings][:env_var][:enabled]
+      #file.write("export AZMON_CLUSTER_COLLECT_ENV_VAR=#{parsedconfig[:log_collection_settings][:env_var][:enabled]}\n")
+      puts "Using config map setting for cluster level environment variable collection"
+    end
+    # Close file after writing all environment variables
+    file.close
+  end
+rescue => errorStr
+  puts "Exception in reading config file, using defaults"
+end
+
 file = File.open("config_env_var.txt", "w")
 
 if !file.nil?
-  begin
-    if !parsedconfig.nil? && !parsedconfig[:log_collection_settings].nil?
-      #Get stdout log config settings
-      if !parsedconfig[:log_collection_settings][:stdout].nil? && !parsedconfig[:log_collection_settings][:stdout][:enabled].nil?
-        collectStdoutLogs = parsedconfig[:log_collection_settings][:stdout][:enabled]
-        #file.write("export AZMON_COLLECT_STDOUT_LOGS=#{collectStdoutLogs}\n")
-		puts "Using config map setting for stdout log collection"
-        if parsedconfig[:log_collection_settings][:stdout][:enabled] && !parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces].nil?
-          stdoutExcludeNamespaces = parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces]
-          #file.write("export AZMON_STDOUT_EXCLUDED_NAMESPACES=#{parsedconfig[:log_collection_settings][:stdout][:exclude_namespaces]}\n")
-		  puts "Using config map setting for stdout log collection to exclude namespace"
-        end
-      end
-      #Get stderr log config settings
-      if !parsedconfig[:log_collection_settings][:stderr].nil? && !parsedconfig[:log_collection_settings][:stderr][:enabled].nil?
-        collectStdErrLogs = parsedconfig[:log_collection_settings][:stderr][:enabled]
-        #file.write("export AZMON_COLLECT_STDERR_LOGS=#{parsedconfig[:log_collection_settings][:stderr][:enabled]}\n")
-		 puts "Using config map setting for stderr log collection"
-        if parsedconfig[:log_collection_settings][:stderr][:enabled] && !parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces].nil?
-          stderrExcludeNamespaces = parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces]
-          #file.write("export AZMON_STDERR_EXCLUDED_NAMESPACES=#{parsedconfig[:log_collection_settings][:stderr][:exclude_namespaces]}\n")
-		   puts "Using config map setting for stderr log collection to exclude namespace"
-        end
-      end
-      #Get environment variables log config settings
-      if !parsedconfig[:log_collection_settings][:env_var].nil? && !parsedconfig[:log_collection_settings][:env_var][:enabled].nil?
-        collectClusterEnvVariables = parsedconfig[:log_collection_settings][:env_var][:enabled]
-        #file.write("export AZMON_CLUSTER_COLLECT_ENV_VAR=#{parsedconfig[:log_collection_settings][:env_var][:enabled]}\n")
-		puts "Using config map setting for cluster level environment variable collection"
-      end
-      # Close file after writing all environment variables
-      file.close
-    end
-  rescue => errorStr
-    puts "Exception in reading config file, using defaults"
-  end
 else
   puts "Exception while opening file for writing config environment variables"
 end
