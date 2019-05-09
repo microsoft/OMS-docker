cbf95ff46c3f6ee4be5312f58cd67241ba0d2a71
rashmichandrashekar <rashmy@microsoft.com>
2019-05-08 19:31:58 -0700

    changes

--- ci_feature/main.sh
+++ ci_feature/main.sh
@@ -61,13 +61,19 @@ else
 fi
 
 #Parse the configmap to set the right environment variables
-/opt/microsoft/omsagent/ruby/bin/ruby tomlparser.rb
+#/opt/microsoft/omsagent/ruby/bin/ruby tomlparser.rb
 
-cat config_env_var.txt | while read line; do
-    echo $line
-    echo $line >> ~/.bashrc
-done
-source ~/.bashrc
+export AZMON_LOG_TAIL_PATH="/var/log/containers/*.log"
+export AZMON_LOG_EXCLUSION_REGEX_PATTERN=stdout
+
+echo "export AZMON_LOG_TAIL_PATH=\"/var/log/containers/*.log\"" > ~/.bashrc
+echo "export AZMON_LOG_EXCLUSION_REGEX_PATTERN=stdout" > ~/.bashrc
+
+#cat config_env_var.txt | while read line; do
+#    echo $line
+#    echo $line >> ~/.bashrc
+#done
+#source ~/.bashrc
 
 #Commenting it for test. We do this in the installer now
 #Setup sudo permission for containerlogtailfilereader
