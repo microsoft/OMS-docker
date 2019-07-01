f1f831e6b711a6727de33cf2af7979020346b4f8
rashmy <rashmy@RASHMY-PC2>
2019-07-01 16:54:48 -0700

    changes

--- ci_feature/main.sh
+++ ci_feature/main.sh
@@ -205,61 +205,45 @@ echo "export TELEMETRY_APPLICATIONINSIGHTS_KEY=$aikey" >> ~/.bashrc
 
 source ~/.bashrc
 
-#start telegraf
-#/usr/bin/telegraf --config $telegrafConfFile &
-#if [ ! -e "/etc/config/kube.conf" ]; then
-#Changes to source the right environment variable file for prometheus custom settings
 #Parse the prometheus configmap to create a file with new custom settings.
 /opt/microsoft/omsagent/ruby/bin/ruby tomlparser-prom-customconfig.rb
 
-#If the config parsing was successful, a new file with environment variable for custom settings is created
-if [ -e "/opt/prom_config_env_var" ]; then
-      #Run telegraf in test mode to check if config settings are valid
-      echo "Running telegraf in test mode"
-      if [ ! -e "/etc/config/kube.conf" ]; then
-            /opt/telegraf --config /opt/telegraf-test.conf -test
-      else
-            /opt/telegraf --config /opt/telegraf-test-rs.conf -test
-      fi
-
-      if [ $? -eq 0 ]; then
-            #If exit code is 0, telegraf was started successfully with the custom setting values
-            echo "Running telegraf in test mode succeeded, sourcing custom setting environment variable file"
-            cat prom_config_env_var | while read line; do
-                  echo $line >> ~/.bashrc
-            done
-            source prom_config_env_var
-      else
-            echo "Running telegraf in test mode failed, sourcing default environment variable file"
-            useDefaults=true #set this to true if telegraf exited with non zero exit code in test mode
-      fi
+#If config parsing was successful, a copy of the conf file with replaced custom settings file is created
+if [ ! -e "/etc/config/kube.conf" ]; then
+            if [ -e "/opt/telegraf-test.conf" ]; then
+                  /opt/telegraf --config /opt/telegraf-test.conf -test
+                  if [ $? -eq 0 ]; then
+                        mv "/opt/telegraf-test.conf" "/etc/opt/microsoft/docker-cimprov/telegraf.conf"
+                  fi
+            fi
 else
-      useDefaults=true #set this to true if no file with env variabled for custom settings was created
-fi
-
-if [ "$useDefaults" = true ] ; then
-      if [ ! -e "/etc/config/kube.conf" ]; then
-            cat defaultpromenvvariables | while read line; do
-                  echo $line >> ~/.bashrc
-            done
-            source defaultpromenvvariables
-      else
-            cat defaultpromenvvariables-rs | while read line; do
-                  echo $line >> ~/.bashrc
-            done
-            source defaultpromenvvariables-rs
+      if [ -e "/opt/telegraf-test-rs.conf" ]; then
+                  /opt/telegraf --config /opt/telegraf-test-rs.conf -test
+                  if [ $? -eq 0 ]; then
+                        mv "/opt/telegraf-test-rs.conf" "/etc/opt/microsoft/docker-cimprov/telegraf-rs.conf"
+                  fi
       fi
 fi
 
+#Setting default environment variables to be used in any case of failure in the above steps
+if [ ! -e "/etc/config/kube.conf" ]; then
+      cat defaultpromenvvariables | while read line; do
+            echo $line >> ~/.bashrc
+      done
+      source defaultpromenvvariables
+else
+      cat defaultpromenvvariables-rs | while read line; do
+            echo $line >> ~/.bashrc
+      done
+      source defaultpromenvvariables-rs
+fi
+
+#start telegraf
 /opt/telegraf --config $telegrafConfFile &
 /opt/telegraf --version
 dpkg -l | grep td-agent-bit | awk '{print $2 " " $3}'
-#fi
-
 
 #dpkg -l | grep telegraf | awk '{print $2 " " $3}' 
-
-
 
 shutdown() {
 	/opt/microsoft/omsagent/bin/service_control stop
