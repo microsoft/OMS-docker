0a35b9f4e99dbea5da0f0dda2da94998926c7e0e
Vishwanath Narasimhan <visnara@microsoft.com>
2019-11-21 19:13:12 -0800

    optimizations

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -14,7 +14,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
 
-wget https://dockerprovider.blob.core.windows.net/most/docker-cimprov-8.0.0-7.universal.x86_64.sh
+wget https://dockerprovider.blob.core.windows.net/most/docker-cimprov-8.0.0-8.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 #Extract omsbundle
