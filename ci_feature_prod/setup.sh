5e1d99a0b9829c5000cbf8ca65d61e0ad9cc6231
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-03-10 16:59:27 -0700

    revert back td-agent 0.14.4

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -65,7 +65,7 @@ chmod 777 /opt/telegraf
 wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
 sudo echo "deb https://packages.fluentbit.io/ubuntu/xenial xenial main" >> /etc/apt/sources.list  
 sudo apt-get update
-sudo apt-get install td-agent-bit=1.3.9 -y
+sudo apt-get install td-agent-bit=0.14.4 -y
 
 rm -rf $TMPDIR/omsbundle
 rm -f $TMPDIR/omsagent*.sh
