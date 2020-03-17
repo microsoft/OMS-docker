6c9780f0a2260a362cd0f78e2161906c031387b5
🌞 Vishwanath Narasimhan 🌤 <visnara@microsoft.com>
2020-03-17 00:24:37 -0700

    update image

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -14,7 +14,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
 
-wget https://github.com/microsoft/Docker-Provider/releases/download/v8.0.0.3/docker-cimprov-8.0.0-3.universal.x86_64.sh
+wget https://dockerprovider.blob.core.windows.net/throttling/docker-cimprov-9.0.0-1.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
 
@@ -53,7 +53,7 @@ wget https://github.com/microsoft/Docker-Provider/releases/download/5.0.0.0/tele
 
 chmod 777 /opt/telegraf
 
-/$TMPDIR/docker-cimprov-8.0.0-*.x86_64.sh --install
+/$TMPDIR/docker-cimprov-9.0.0-*.x86_64.sh --install
 
 #download and install fluent-bit(td-agent-bit)
 wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
