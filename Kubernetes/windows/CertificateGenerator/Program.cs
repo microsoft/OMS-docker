7213d7882c4a9dc309af393076bf2647964043ce
bragi92 <kadubey@microsoft.com>
2020-04-22 13:50:43 -0700

    test comment

--- Kubernetes/windows/CertificateGenerator/Program.cs
+++ Kubernetes/windows/CertificateGenerator/Program.cs
@@ -286,6 +286,7 @@ namespace CertificateGenerator
             }
         }
 
+        // Register certificate with Log Analytics
         public static X509Certificate2 RegisterAgentWithOMS(string logAnalyticsWorkspaceId,
             string logAnalyticsWorkspaceKey, string logAnalyticsWorkspaceDomain)
         {
