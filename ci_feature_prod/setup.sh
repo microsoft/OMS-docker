f8d091d88040379e721c39cc81cf169b1457d0fa
🌞 Vishwanath Narasimhan 🌤 <visnara@microsoft.com>
2020-05-04 21:56:54 -0700

    update fluentbit to 1.14.2

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -72,7 +72,7 @@ chmod 777 /opt/telegraf
 wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
 sudo echo "deb https://packages.fluentbit.io/ubuntu/xenial xenial main" >> /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install td-agent-bit=0.14.4 -y
+sudo apt-get install td-agent-bit=1.4.2 -y
 
 rm -rf $TMPDIR/omsbundle
 rm -f $TMPDIR/omsagent*.sh
