e5dda3134a7ac0901f5250d22369d42af271ff07
Dilip Raghunathan <dilipr@microsoft.com>
2020-05-06 16:27:27 -0700

    Adding stepTimeout to tasks

--- Kubernetes/windows/acrWorkflows/acrcreate.ps1
+++ Kubernetes/windows/acrWorkflows/acrcreate.ps1
@@ -6,4 +6,4 @@ az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https:
 
 
 # test task
-az acr task update -n createimagewintestcidev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch dilipr/winakslog --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000  --auth-mode Default --debug
+az acr task create -n createimagewintestcidev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch dilipr/winakslog --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000  --auth-mode Default --debug
