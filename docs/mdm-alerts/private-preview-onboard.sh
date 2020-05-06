40e851c48f31cfb11bf7f4762daacd2c465ce135
rashmy <rashmy@RASHMY-PC2>
2020-05-05 22:46:46 -0700

    changes

--- docs/mdm-alerts/private-preview-onboard.sh
+++ docs/mdm-alerts/private-preview-onboard.sh
@@ -82,6 +82,7 @@ helm repo add azmon-preview-mdm-alert https://rashmichandrashekar.github.io/azur
 echo "updating helm repo to get latest charts"
 helm repo update
 
+echo "uninstall existing release"
 helm uninstall azmon-containers-ci-mdm-alert-release
 
 helm upgrade --install azmon-containers-ci-mdm-alert-release --set omsagent.secret.wsid=$workspaceGuid,omsagent.secret.key=$workspaceKey,omsagent.env.clusterId=$clusterResourceId,omsagent.env.clusterRegion=$clusterRegion azmon-preview-mdm-alert/azuremonitor-containers --kube-context $kubecontext
