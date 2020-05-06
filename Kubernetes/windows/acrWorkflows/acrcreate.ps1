f0729967e7c5f965a2819c7f670eb54a7c4eed74
Dilip Raghunathan <dilipr@microsoft.com>
2020-05-06 15:16:13 -0700

    More PR feedback

---
+++ Kubernetes/windows/acrWorkflows/acrcreate.ps1
@@ -0,0 +1,5 @@
+# create acr task for windows from ci_feature branch to create image in cidev repository
+az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
+
+# create acr task for windows from ci_feature_prod branch to create image in ciprod repository
+az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsprodnamespace.yaml.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
