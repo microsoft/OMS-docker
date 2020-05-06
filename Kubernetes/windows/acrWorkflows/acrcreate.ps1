afd8e2259253defe968e55dfc23c7cad964301d7
Dilip Raghunathan <dilipr@microsoft.com>
2020-05-06 15:26:09 -0700

    test parameterized task

--- Kubernetes/windows/acrWorkflows/acrcreate.ps1
+++ Kubernetes/windows/acrWorkflows/acrcreate.ps1
@@ -3,3 +3,7 @@ az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https:
 
 # create acr task for windows from ci_feature_prod branch to create image in ciprod repository
 az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsprodnamespace.yaml.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
+
+
+# test task
+az acr task update -n createimagewintestcidev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch dilipr/winakslog --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000  --auth-mode Default --debug
