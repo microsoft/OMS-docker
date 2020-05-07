ded8517314b281ac5f52055b435efc852e089d0c
Dilip Raghunathan <dilipr@microsoft.com>
2020-05-06 22:44:54 -0700

    Updating acr task creation script

--- Kubernetes/windows/acrWorkflows/acrcreate.ps1
+++ Kubernetes/windows/acrWorkflows/acrcreate.ps1
@@ -1,9 +1,10 @@
 # create acr task for windows from ci_feature branch to create image in cidev repository
-az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
+az acr task create -n createimagewincifeaturecidev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
 
 # create acr task for windows from ci_feature_prod branch to create image in ciprod repository
-az acr task create -n createimagecifeaturedev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsprodnamespace.yaml.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
+az acr task create -n createimagewincifeatureciprod -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch ci_feature --file Kubernetes/windows/acrWorkflows/acrwindowsprodnamespace.yaml.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
 
 
 # test task
-az acr task create -n createimagewintestcidev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch dilipr/winakslog --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 347568efa0de1e222d6179fe6c4a8035cfcf4724  --auth-mode Default --debug
+az acr task create -n createimagewintestcidev -r containerinsightsprod -c https://github.com/Microsoft/OMS-docker.git --branch dilipr/winakslog --file Kubernetes/windows/acrWorkflows/acrwindowsdevnamespace.yaml --commit-trigger-enabled true --platform Windows/amd64 --git-access-token 00000000000000000000000000 --auth-mode Default --debug
+
