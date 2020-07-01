a440c3ded3f7d47fd399590a6a8220e29d28a84d
Dilip Raghunathan <dilipr@microsoft.com>
2020-06-30 17:05:58 -0700

    JSON Parsing hotfix

--- ci_feature_prod/setup.sh
+++ ci_feature_prod/setup.sh
@@ -14,7 +14,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
 
-wget https://github.com/microsoft/Docker-Provider/releases/download/10.0.0-1/docker-cimprov-10.0.0-1.universal.x86_64.sh
+wget https://github.com/microsoft/Docker-Provider/releases/download/10.0.0-2/docker-cimprov-10.0.0-2.universal.x86_64.sh
 
 chmod 775 $TMPDIR/*.sh
 
