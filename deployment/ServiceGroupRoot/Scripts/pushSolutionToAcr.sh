d66e5b49e76b7b5f1497ad09ffba929912293a34
sarahpeiffer <speiffer@wustl.edu>
2021-10-20 12:50:33 -0700

    remove sudo

--- deployment/ServiceGroupRoot/Scripts/pushSolutionToAcr.sh
+++ deployment/ServiceGroupRoot/Scripts/pushSolutionToAcr.sh
@@ -14,11 +14,11 @@ if [ -z $IMAGE_TAG ]; then
 fi
 
 echo "installing podman"
-echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_'"$(lsb_release -sr)"'/ /' | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_"$(lsb_release -sr)"/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_stable.gpg > /dev/null
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install podman -y
+echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_'"$(lsb_release -sr)"'/ /' | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_"$(lsb_release -sr)"/Release.key | gpg --dearmor |  tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_stable.gpg > /dev/null
+apt-get update -y
+apt-get upgrade -y
+apt-get install podman -y
 export podmanVersion="$(echo $( podman version --format '{{.Version}}'))"
 
 if [ ! -z "$podmanVersion" ]; then
